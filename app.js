560d2acc93bf5a50737f41e586e0d6a9f5e7f75c
Joshua Dotto <joshdotto85.github@gmail.com>
2023-11-02 00:25:32 -0500

    update app entry point

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {cors: {origin: '*'}});
+
+const { createMqttClient } = require('./models/mqttData');
+
+createMqttClient(io);
+
 require('dotenv').config();
 const port = process.env.PORT;
 
-const express = require('express');
 const HomeController = require('./controllers/homeController');
 const ProjectsController = require('./controllers/projectsController');
-const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -13,6 +22,6 @@ app.get('/projects', ProjectsController.getProjectsPage);
 app.get('/project1', ProjectsController.getProject1Page);
 app.get('/project2', ProjectsController.getProject2Page);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
