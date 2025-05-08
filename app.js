a0cffcf0db64ae3a1130b3b02f3dc46a4846dc4e
Joshua Dotto <joshdotto85.github@gmail.com>
2025-05-08 15:17:19 +0000

    added body parser for json data

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {cors: {origin: '*'}});
+const bodyParser = require('body-parser');
 
 const { createMqttClient } = require('./models/mqttData');
+const { receiveSensorData } = require('./controllers/sensorDataController');
 
 createMqttClient(io);
 
@@ -16,11 +18,15 @@ const HomeController = require('./controllers/homeController');
 const ProjectsController = require('./controllers/projectsController');
 
 app.use(express.static('public'));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.get('/', HomeController.getHomePage);
 app.get('/projects', ProjectsController.getProjectsPage);
 app.get('/project1', ProjectsController.getProject1Page);
 app.get('/project2', ProjectsController.getProject2Page);
+app.post('/sensor-data', (req, res) => receiveSensorData(req, res, io));
 
 server.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
